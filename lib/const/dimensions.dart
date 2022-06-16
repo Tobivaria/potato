59ae3c295c6e009af0185a36f4737115887ce8a2
Tobias Schröpfer <tobias-schroepfer@web.de>
2022-06-16 18:32:38 +0200

    Add translate button

--- lib/const/dimensions.dart
+++ lib/const/dimensions.dart
@@ -1,19 +1,21 @@
 class Dimensions {
   // translation table
   static const double tableHeaderHeight = 50.0;
-  static const double tableRowMinHeight = 64.0;
+  static const double tableRowMinHeight = arbButtonsHeight;
 
   static const double arbSettingIconSize = 14.0;
 
   static const double idCellWidth = 356;
   static const double idTextfieldWidth = 300;
-  static const double arbOptionOffset =
-      26.0; // distance between the remove icon and the option textfield
+
+  // distance between the remove icon and the option textfield
+  static const double arbOptionOffset = 26.0;
 
   // used for calculating the cell heights
   static const double arbKeyHeight = 32.0;
   static const double arbDescriptionHeight = 32.0;
   static const double arbPlaceholderHeight = 64.0;
+  static const double arbButtonsHeight = 78.0;
   static const double paddingBetweenArbOptions = 8.0;
 
   static const double languageCellWidth = 320;
--- lib/translation/translation_entry.dart
+++ lib/translation/translation_entry.dart
@@ -37,7 +37,7 @@ class _TranslationEntryState extends ConsumerState<TranslationEntry> {
   @override
   void initState() {
     super.initState();
-    _controller.text = widget.translation ?? '';
+    _initializeFields();
     _focus.addListener(_updateTranslation);
   }
 
@@ -58,7 +58,14 @@ class _TranslationEntryState extends ConsumerState<TranslationEntry> {
       // postpone rebuilding after build finished
       WidgetsBinding.instance
           .addPostFrameCallback((_) => _validateTranslation());
+    } else if (oldWidget.translation != widget.translation) {
+      // postpone rebuilding after build finished
+      WidgetsBinding.instance.addPostFrameCallback((_) => _initializeFields());
     }
+  }
+
+  void _initializeFields() {
+    _controller.text = widget.translation ?? '';
   }
 
   // update the translation key, once the textfield is losing focus
